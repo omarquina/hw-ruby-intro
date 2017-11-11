ed5bf066c4f49f0a4c610f4996223b917b61b037
Orlando <marquina_o@yahoo.es>
2017-11-11 07:58:15 -0400

    todas las pruebas pasan

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -39,14 +39,68 @@ end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+	return false if s.empty?
+	#puts "no es vacio"
+	return false unless s[0] =~ /[a-zA-Z]/
+	#puts "no es letra"
+	char = s[0].ord
+	vocales = %w{a e i o u A E I O U}.map{|vocal| vocal.ord}
+	return false if vocales.include?(char)
+	#puts "no es consonante"
+	true
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  return false if s.empty?
+  valid = %w{0 1}
+  #puts "#{s} =~ /[^0-1] #{(s =~ /[^0-1]/)}"
+  return false if s =~ /[^0-1]/
+  sum =0
+  s2 = s.reverse
+  #puts "S tiene 0s y 1s: #{s}, lenght: #{s.length}"
+  #length=s.length-1
+  #(length).downto(-1) do |index|
+  #  potencia=length-index
+  #  puts "INDEX: #{index}, s[#{index}]: #{s[index]}"
+  #  sum += s[index].to_i*(2**potencia)
+  #  puts "SUm : #{sum}"
+  #end
+
+  s2 = s.reverse
+  s2.length.times do |index|
+    #puts "INDEX: #{index}, s[#{index}]: #{s2[index]}"
+    sum += s2[index].to_i*(2**index)
+    #puts "SUm : #{sum}"
+  end
+  return false if sum % 4 != 0
+  true
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  attr_accessor :isbn,:price
+  def initialize isbn,price
+    self.isbn = isbn
+    self.price = price
+  end
+
+  def isbn=(value)
+    puts "isbn= value: #{value}"
+    raise ArgumentError.new("Must specify a value") if value.empty?
+    @isbn=value
+  end
+
+  def price=(value)
+    raise ArgumentError.new("") if value.zero? or value.negative?
+    @price = value
+  end
+  #puts BookInStock.instance_methods.sort.to_s
+  #puts BookInStock.instance_method(:"isbn=").inspect
+  def price_as_string
+	  sprintf("$%.2f",price)
+
+  end
 end
